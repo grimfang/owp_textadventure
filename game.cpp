f6dc64a925bf0795f23ea8e4c134b06d1f3a7351
fireclaw <fireclawthefox@gmail.com>
2015-01-03 14:56:48 +0100

    Finish decission chooser
    implement functionality to read and write decissions of story files
    implement selections of possible decissions in the story

--- game.cpp
+++ game.cpp
@@ -90,8 +90,6 @@ int Game::show() {
                 enemies[current_id] = enemy;
             } else {
                 current_id = stoi(values[0]);
-                cout << values[0];
-                cout << values[1] + "\n";
                 if(values[1].empty()) values[1] = " ";
                 story[current_id] += values[1] + "\n";
             }
@@ -113,23 +111,37 @@ int Game::show() {
             //nothing to do here
             //cout << "this place is safe, no enemies here" << endl;
         }
+
+        bool has_decissions = false;
         try {
-            bool add_newline = false;
-            //map<int, int, string> display_decissions;
+            map<int, int> display_decissions;
+            int number_decissions = 0;
             for(unsigned int i = 0; i < decissions[current_story_point].size(); i++) {
-                /*vector<string> decs = split(decissions[current_story_point], '>');
-                display_decissions[i][decs[0]] = decs[1]*/
-                cout << i << ") " << decissions[current_story_point][i] << " | ";
-                add_newline = true;
+                vector<string> decs = split(decissions[current_story_point][i], '>');
+                display_decissions[i] = stoi(decs[1]);
+                cout << i << ") " << decs[0] << "    ";
+                has_decissions = true;
+                number_decissions = i;
             }
-            if (add_newline) {
+            if (has_decissions) {
+                string user_input;
+                int choice;
                 cout << endl;
+                cout << "> ";
+                cin >> user_input;
+                if (choice > number_decissions) choice = 0;
+                else if (choice < number_decissions) choice = 0;
+                choice = stoi(user_input);
+                current_story_point = display_decissions[choice];
             }
         } catch (out_of_range e) {
             //nothing to do here
         }
 
-        current_story_point += 1;
+        if (!has_decissions) {
+            current_story_point += 1;
+        }
+
         if(current_story_point >= story.size()) {
             end = true;
         }
